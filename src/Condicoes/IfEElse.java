b8973d1a69727696ed45c00e513afceb2cfb69d8
gabrielmamedee <mamede.dev@gmail.com>
2025-07-25 17:24:41 -0300

    aula condicionais

---
+++ src/Condicoes/IfEElse.java
@@ -0,0 +1,34 @@
+package Condicoes;
+
+public class IfEElse {
+    public static void main(String[] args) {
+
+        /*
+        * IF e ELSE - Condições
+        * ELSE IF
+        * Objetivo: Passar o ninja de nível de acordo com o número de missoes
+        * */
+
+        String nome = "Naruto Uzumaki";
+        String rank;
+
+        int idade = 16;
+        boolean hokage = false;
+        short numeroDeMissoes = 10;
+
+        /*
+        * if (condicao) { resultado }
+        * else if (condicao) { resultado }
+        * else { resultado caso nada seja verdadeiro}
+        * */
+
+        //se (condicao) {faca isso}
+        if (numeroDeMissoes >= 10 && idade > 15) {
+            System.out.println("Rank: Chunnin");
+        } else if (numeroDeMissoes >= 20){
+            System.out.println("Rank: Jounin");
+        } else {
+            System.out.println("Rank: Gennim");
+        }
+    }
+}
