0268054c908a1fd3441579bf641ca364c6cc1db6
gabrielmamedee <mamede.dev@gmail.com>
2025-07-29 16:15:49 -0300

    estudo sobre array

---
+++ src/Array/Array.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class Array {
+    public static void main(String[] args) {
+        String[] ninja = new String[4];
+
+        ninja[0] = "Naturo Uzumaki";
+        ninja[1] = "Sakura Haruno";
+        ninja[2] = "Sasuke Uchina";
+        ninja[3] = "Hinata Hyuga";
+
+        System.out.println(ninja[1]);
+
+        for (int i = 0; i < ninja.length; i++) {
+            System.out.println(ninja[i]);
+        }
+    }
+}
