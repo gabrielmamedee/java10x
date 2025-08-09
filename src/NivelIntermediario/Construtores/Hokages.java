b2776fe65e614269a29524856ea0f1d7c0919f8f
gabrielmamedee <mamede.dev@gmail.com>
2025-08-09 13:08:01 -0300

    Construtores

---
+++ src/NivelIntermediario/Construtores/Hokages.java
@@ -0,0 +1,19 @@
+package NivelIntermediario.Construtores;
+
+public class Hokages {
+
+    String nome;
+    int idade;
+    boolean vivoOuNao;
+
+    // All args constructor = Construtor com todos os argumentos.
+    public Hokages(String nome, int idade, boolean vivoOuNao) {
+        this.nome = nome;
+        this.idade = idade;
+        this.vivoOuNao = vivoOuNao;
+    }
+
+    //No args constructor
+    public Hokages() {
+    }
+}
---
+++ src/NivelIntermediario/Construtores/Main.java
@@ -0,0 +1,9 @@
+package NivelIntermediario.Construtores;
+
+public class Main {
+    public static void main(String[] args) {
+        Hokages Minato = new Hokages("Minato Namikaze", 32, false);
+        System.out.println(Minato.idade);
+
+    }
+}
